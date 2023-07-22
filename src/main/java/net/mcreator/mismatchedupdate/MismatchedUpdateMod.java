8de1ad85993238f24663d02744d1a60b7e31c4b5
Badpig Studios <badpi@Badpig-pin>
2023-07-22 15:37:09 -0500

    geckolib addition

--- src/main/java/net/mcreator/mismatchedupdate/MismatchedUpdateMod.java
+++ src/main/java/net/mcreator/mismatchedupdate/MismatchedUpdateMod.java
@@ -13,6 +13,8 @@
  */
 package net.mcreator.mismatchedupdate;
 
+import software.bernie.geckolib.GeckoLib;
+
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
@@ -58,6 +60,7 @@ public class MismatchedUpdateMod {
 
 		MismatchedUpdateModFeatures.REGISTRY.register(bus);
 
+		GeckoLib.initialize();
 	}
 
 	private static final String PROTOCOL_VERSION = "1";
