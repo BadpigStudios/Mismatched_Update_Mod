245718b4197d02074f0cffe145b39183c421e812
Badpig Studios <badpi@Badpig-pin>
2023-07-21 23:46:47 -0500

    EB item fixes

--- src/main/java/net/mcreator/mismatchedupdate/block/ExplosiveBerryBlock.java
+++ src/main/java/net/mcreator/mismatchedupdate/block/ExplosiveBerryBlock.java
@@ -1,6 +1,7 @@
 
 package net.mcreator.mismatchedupdate.block;
 
+import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.block.state.BlockState;
@@ -18,12 +19,14 @@ import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
 
 import net.mcreator.mismatchedupdate.procedures.ExplosiveBerryPlantDestroyedByPlayerProcedure;
+import net.mcreator.mismatchedupdate.init.MismatchedUpdateModItems;
 
 import java.util.List;
+import java.util.Collections;
 
 public class ExplosiveBerryBlock extends FlowerBlock {
 	public ExplosiveBerryBlock() {
-		super(() -> MobEffects.HARM, 1, BlockBehaviour.Properties.of(Material.PLANT).sound(SoundType.GRASS).instabreak().noLootTable().noCollission());
+		super(() -> MobEffects.HARM, 1, BlockBehaviour.Properties.of(Material.PLANT).sound(SoundType.GRASS).instabreak().noCollission());
 	}
 
 	@Override
@@ -45,6 +48,14 @@ public class ExplosiveBerryBlock extends FlowerBlock {
 	@Override
 	public int getFireSpreadSpeed(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
 		return 60;
+	}
+
+	@Override
+	public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+		List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+		if (!dropsOriginal.isEmpty())
+			return dropsOriginal;
+		return Collections.singletonList(new ItemStack(MismatchedUpdateModItems.EXPLOSIVE_BERRY_PROJECTIL.get(), 5));
 	}
 
 	@Override
